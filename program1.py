b91192da35b691a28f16b3c74611e9c7f3f29b73
infinity-code-max <156830363+infinity-code-max@users.noreply.github.com>
2024-09-27 14:19:48 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,34 +1,13 @@
-def ispar(s):
-    stack = []
-    for char in s:
-      
-        # Opening bracket
-        if char in '({[':
-            stack.append(char)
-            
-        # Closing Bracket    
-        elif char in ')}]':
-          
-            # closing bracket without opening
-            if not stack:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        d = {'()', '[]', '{}'}
+        for c in s:
+            if c in '({[':
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
                 return False
-              
-            # Else pop an item check for matching  
-            top = stack.pop()
-            if (top == '(' and char != ')') or \
-               (top == '{' and char != '}') or \
-               (top == '[' and char != ']'):
-                return False
-              
-    # If an opening bracket without closing          
-    return len(stack) == 0
-
-s = '{()}[]'
-
-if ispar(s):
-    print("true")
-else:
-    print("false")
+        return not stk
 
 
 
