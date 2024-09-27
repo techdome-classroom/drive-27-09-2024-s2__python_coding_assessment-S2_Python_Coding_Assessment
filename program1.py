84793d7a7a7cda3e4827cc808482aeddc19aa242
infinity-code-max <156830363+infinity-code-max@users.noreply.github.com>
2024-09-27 14:07:43 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,10 +1,34 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+def ispar(s):
+    stack = []
+    for char in s:
+      
+        # Opening bracket
+        if char in '({[':
+            stack.append(char)
+            
+        # Closing Bracket    
+        elif char in ')}]':
+          
+            # closing bracket without opening
+            if not stack:
+                return False
+              
+            # Else pop an item check for matching  
+            top = stack.pop()
+            if (top == '(' and char != ')') or \
+               (top == '{' and char != '}') or \
+               (top == '[' and char != ']'):
+                return False
+              
+    # If an opening bracket without closing          
+    return len(stack) == 0
+
+s = '{()}[]'
+
+if ispar(s):
+    print("true")
+else:
+    print("false")
 
 
 
